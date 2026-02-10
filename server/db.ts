c2426e42050fcc095e60fa2eb678abe8e1f32e24
Yossi Hadad <yossihd3000@gmail.com>
2026-02-11 00:58:53 +0200

    Use SSL for RDS PostgreSQL connection

--- server/db.ts
+++ server/db.ts
@@ -9,9 +9,13 @@ if (!connectionString) {
 }
 console.log('Connecting to database:', connectionString.replace(/:[^:@]+@/, ':***@'));
 
+// RDS (and most cloud Postgres) require SSL. Use SSL when not connecting to localhost.
+const isLocalDb = /localhost|127\.0\.0\.1/.test(connectionString);
+const sslConfig = isLocalDb ? false : { rejectUnauthorized: false };
+
 export const pool = new Pool({
   connectionString,
-  ssl: false,
+  ssl: sslConfig,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
@@ -23,7 +27,7 @@ pool.on('error', (err) => {
 });
 
 pool.on('connect', () => {
-  console.log('Successfully connected to LOCAL PostgreSQL database');
+  console.log('Successfully connected to PostgreSQL database');
 });
 
 export const db = drizzle(pool, { schema });
